2a5cd8e99facece1f8a47d36a76d0989f8a2d9bb
jinseo08 <kjs_8086@naver.com>
2022-07-18 08:59:13 +0900

    first commit

---
+++ src/main/java/com/its/happy/HappyApplication.java
@@ -0,0 +1,13 @@
+package com.its.happy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/its/happy/HappyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.happy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
