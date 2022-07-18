7376889fc3ca0cd15ed910b158078587ca76e2a1
jinseo08 <kjs_8086@naver.com>
2022-07-18 11:43:56 +0900

    point 클래스 기본세팅

--- src/main/java/com/its/happy/entity/PointEntity.java
+++ src/main/java/com/its/happy/entity/PointEntity.java
@@ -1,4 +1,27 @@
 package com.its.happy.entity;
 
-public class PointEntity {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "point_table")
+public class PointEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id")
+    private Long pointId;
+
+    @Column(name = "point_value",nullable = false)
+    private String pointValue;
+
+    // 적립금(n)이 회원(1)을 참조함
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity memberEntity;
+
+
 }
