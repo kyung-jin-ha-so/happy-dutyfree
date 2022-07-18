c58725d21e8cf9eb3c41975f1caf8a6287a24afb
jinseo08 <95058698+jinseo08@users.noreply.github.com>
2022-07-18 12:24:54 +0900

    회원,적립금 관련 (#3)
    member 클래스 기본세팅,point 클래스 기본세팅,MemberEntity 참조관계 추가,Admin 기본 패키지 구성

--- src/main/java/com/its/happy/dto/PointDTO.java
+++ src/main/java/com/its/happy/dto/PointDTO.java
@@ -1,4 +1,21 @@
 package com.its.happy.dto;
 
+import com.its.happy.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PointDTO {
+
+    private Long pointId;
+    private Long memberId;
+    private String pointValue;
+    private LocalDateTime createdTime;
+
 }
---
+++ src/main/java/com/its/happy/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.its.happy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+}
