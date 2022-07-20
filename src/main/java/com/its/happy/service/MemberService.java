130b0e960af7636e3bc5a6871c3e815d77f98890
jinseo08 <95058698+jinseo08@users.noreply.github.com>
2022-07-20 16:49:55 +0900

    member 관련 (#14)
    * member 비밀번호 암호화
    * login.html 양식수정 및 추가
    * 아이디찾기, 비밀번호 찾기 html 양식 추가
    * member 로그인 수정(비밀번호 암호화)
    * 문자인증 라이브러리 추가, 멤버 주석 등 수정

--- src/main/java/com/its/happy/service/MemberService.java
+++ src/main/java/com/its/happy/service/MemberService.java
@@ -4,8 +4,11 @@ import com.its.happy.dto.MemberDTO;
 import com.its.happy.entity.MemberEntity;
 import com.its.happy.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.plaf.PanelUI;
 import java.util.Optional;
 
 @Service
@@ -13,34 +16,49 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
+
+    // 회원가입 구현 - 비밀번호 암호화
+    @Transactional
     public void save(MemberDTO memberDTO) {
+        String password = memberDTO.getMemberPassword();
+        String encodedPassword = passwordEncoder.encode(password);
+        memberDTO.setMemberPassword(encodedPassword);
         MemberEntity memberEntity = MemberEntity.toSave(memberDTO);
         memberRepository.save(memberEntity);
     }
 
+
+    // 이메일 중복체크
     public String duplicateCheck(String memberEmail) {
         Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(memberEmail);
-        if(optionalMemberEntity.isEmpty()){
+        if (optionalMemberEntity.isEmpty()) {
             return "OK";
-        }else {
+        } else {
             return "NO";
         }
     }
 
+    // 로그인 구현
     public MemberDTO login(MemberDTO memberDTO) {
         Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
-        if(optionalMemberEntity.isPresent()){
+        if (optionalMemberEntity.isPresent()) {
             MemberEntity loginEntity = optionalMemberEntity.get();
-            if(loginEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+//            if(loginEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+            if (passwordEncoder.matches(memberDTO.getMemberPassword(), loginEntity.getMemberPassword())) {
                 return MemberDTO.toMemberDTO(loginEntity);
-            }else {
-                return null; //비밀번호 불일치
+            } else {
+                return null; //비밀번호 불일치시 null 리턴
             }
         } else {
-            return null; //해당 계정이 없음
+            return null; //해당 계정이 없음시 null 리턴
         }
     }
 
 
+
+
 }
+
+
