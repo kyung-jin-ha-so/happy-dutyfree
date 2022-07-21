c19f9f3d94b57a70be3af818d5323bc7bf7ce7ff
ktaekim2 <ktaekim2@gmail.com>
2022-07-21 16:53:12 +0900

    - 항공api ApiExplorer 구현 - ApiExplorer에서 xml을 json 타입으로 data를 받음 - 항공api 테스트코드 작성: json을 양파껍질 까듯이 까서 원하는 정보가 들어있는 Array를 가져오는데 성공.

---
+++ src/main/java/com/its/happy/api/ApiExplorer.java
@@ -0,0 +1,46 @@
+package com.its.happy.api;/* Java 1.8 샘플 코드 */
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ApiExplorer {
+    public static String getFlight() throws IOException, JSONException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.airport.co.kr/service/rest/FlightScheduleList/getIflightScheduleList"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=nLzuj5x1fefaIpwYrFwshTQfLrE28oSxxolY7w0e1J8mAUnAz8XESaS022qnqUW2RHnzr4bi4kM4aIVVnHlNRQ%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("schDate", "UTF-8") + "=" + URLEncoder.encode("20220721", "UTF-8")); /*검색일자*/
+        urlBuilder.append("&" + URLEncoder.encode("schDeptCityCode", "UTF-8") + "=" + URLEncoder.encode("FUK", "UTF-8")); /*출발 도시 코드*/
+        urlBuilder.append("&" + URLEncoder.encode("schArrvCityCode", "UTF-8") + "=" + URLEncoder.encode("ICN", "UTF-8")); /*도착 도시 코드*/
+        urlBuilder.append("&" + URLEncoder.encode("schAirLine", "UTF-8") + "=" + URLEncoder.encode("KE", "UTF-8")); /*항공편 코드*/
+        urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*항공편 넘버*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+
+        return sb.toString();
+    }
+}
