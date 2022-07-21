c0755e4946378927e57e2a01e50da2294527d4f0
jinseo08 <kjs_8086@naver.com>
2022-07-21 15:12:13 +0900

    member 회원가입시 문자인증

--- src/main/java/com/its/happy/controller/MemberController.java
+++ src/main/java/com/its/happy/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.its.happy.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -69,31 +70,10 @@ public class MemberController {
         return "redirect:/";
     }
 
-//    //핸드폰 문자 인증
-//    @PostMapping("/mobileCheck")
-//    public @ResponseBody String mobilCheck(@RequestParam String memberMobile) throws CoolsmsException){
-//        String api_key = "NCSHONZYXFYSSMEB";
-//        String api_secret = "UNNGPMH4FUXIRCPQY2VSJPZFOZTB80QE";
-//        Message coolsms = new Message(api_key, api_secret);
-//
-//        Random rand  = new Random();
-//        String numStr = "";
-//        for(int i=0; i<4; i++) {
-//            String ran = Integer.toString(rand.nextInt(10));
-//            numStr+=ran;
-//        }
-//
-//        HashMap<String, String> params = new HashMap<String, String>();
-//        params.put("to", "01072248086");    // 수신전화번호 (ajax로 view 화면에서 받아온 값으로 넘김)
-//        params.put("from", "01072248086");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
-//        params.put("type", "sms");
-//        params.put("text", "인증번호는 [" + numStr + "] 입니다.");
-//
-//        coolsms.send(params); // 메시지 전송
-//
-//        return numStr;
-//    }
-
-
+    // 핸드폰 문자 인증
+    @GetMapping("/sendSMS")
+    public @ResponseBody String sendSMS(@RequestParam String memberMobile) throws CoolsmsException {
+        return memberService.sendSMS(memberMobile);
+    }
 
 }
