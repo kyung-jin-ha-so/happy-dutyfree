2789183f62b5cf358d83d5cd7e730152c2b808fa
jinseo08 <95058698+jinseo08@users.noreply.github.com>
2022-07-22 16:28:54 +0900

    member 아이디찾기 구현 (#19)
    * memeber 아이디찾기 구현

--- src/main/java/com/its/happy/controller/MemberController.java
+++ src/main/java/com/its/happy/controller/MemberController.java
@@ -2,11 +2,12 @@ package com.its.happy.controller;
 
 import com.its.happy.dto.MemberDTO;
 import com.its.happy.service.MemberService;
+import com.its.happy.service.PointService;
 import lombok.RequiredArgsConstructor;
-import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
-import org.json.simple.JSONObject;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Random;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +23,7 @@ import java.util.Random;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PointService pointService;
 
     // 회원가입 페이지 이동
     @GetMapping("/save")
@@ -33,7 +34,9 @@ public class MemberController {
     // 회원가입 구현
     @PostMapping("/save")
     public String save(@ModelAttribute MemberDTO memberDTO){
-        memberService.save(memberDTO);
+        Long savedId = memberService.save(memberDTO);
+        // 회원가입시 적립금 100만원 저장
+        pointService.save(savedId);
         return "/memberPages/login";
     }
 
@@ -74,6 +77,37 @@ public class MemberController {
     @GetMapping("/sendSMS")
     public @ResponseBody String sendSMS(@RequestParam String memberMobile) throws CoolsmsException {
         return memberService.sendSMS(memberMobile);
+    }
+
+    //아이디찾기 화면 이동
+    @GetMapping("/findEmail")
+    public String findEmailForm(){
+        return "/memberPages/findEmail";
+    }
+    
+
+    // 아이디찾기 - 핸드폰번호로 일치하는 회원 찾기
+    @PostMapping("/mobile-check")
+    public @ResponseBody String mobileCheck(@RequestParam String memberMobile){
+        String mobileResult = memberService.mobileCheck(memberMobile);
+        return mobileResult;
+    }
+
+    // 핸드폰 인증 완료시 해당 전화번호를 가지고 있는 이메일 보여주기
+    @GetMapping("/findEmailResult")
+    public String findEmail(@RequestParam String memberMobile, Model model){
+        System.out.println("MemberController.findEmail");
+        System.out.println(memberMobile);
+        MemberDTO memberDTO = memberService.findEmail(memberMobile);
+        System.out.println(memberDTO);
+        model.addAttribute("member",memberDTO);
+        return "memberPages/findEmailResult";
+    }
+
+    //비밀번호찾기 화면 이동
+    @GetMapping("/findPassword")
+    public String findPasswordForm(){
+        return "/memberPages/findPassword";
     }
 
 }
--- src/main/java/com/its/happy/service/PointService.java
+++ src/main/java/com/its/happy/service/PointService.java
@@ -1,9 +1,31 @@
 package com.its.happy.service;
 
+import com.its.happy.dto.PointDTO;
+import com.its.happy.entity.MemberEntity;
+import com.its.happy.entity.PointEntity;
+import com.its.happy.repository.MemberRepository;
+import com.its.happy.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
+
+    private final PointRepository pointRepository;
+    private final MemberRepository memberRepository;
+
+    // 회원가입시 적립금 100만원 save
+    public void save(Long savedId) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(savedId);
+        if (optionalMemberEntity.isPresent()) {
+            MemberEntity memberEntity = optionalMemberEntity.get();
+            PointEntity pointEntity = PointEntity.toSave(memberEntity);
+            pointRepository.save(pointEntity);
+        }
+    }
+
+
 }
