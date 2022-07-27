80a904b4f6b4f47f5aa69f8f7ea95a590e6491ba
minha723 <minhakim0723@naver.com>
2022-07-27 13:11:15 +0900

    상품 후기 기능 테스트

--- src/main/java/com/its/happy/service/ReviewService.java
+++ src/main/java/com/its/happy/service/ReviewService.java
@@ -1,11 +1,28 @@
 package com.its.happy.service;
 
+import com.its.happy.dto.ReviewDTO;
+import com.its.happy.entity.ProductEntity;
+import com.its.happy.entity.ReviewEntity;
 import com.its.happy.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+
+    public ReviewEntity findById(Long orderProductId) {
+        Optional<ReviewEntity> optionalReviewEntity = reviewRepository.findById(orderProductId);
+        if(optionalReviewEntity.isPresent()){
+            ReviewEntity reviewEntity = optionalReviewEntity.get();
+            return reviewEntity;
+        }return null;
+    }
+
+//    public List<ReviewDTO> findByMemberId(Long memberId) {
+//    }
 }
