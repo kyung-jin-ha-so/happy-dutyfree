316a69ce937300f2945f7015445dbe59ec78c46b
minha723 <99456861+minha723@users.noreply.github.com>
2022-07-27 16:39:51 +0900

    상품 후기관련/상품 목록 페이지관련 (#32)
    * 상품 페이지 관련수정 (페이지 0으로 나오는 부분 수정)
    * 상품 후기 (작성 테스트/삭제 테스트) 추가

--- src/main/java/com/its/happy/repository/ReviewRepository.java
+++ src/main/java/com/its/happy/repository/ReviewRepository.java
@@ -1,7 +1,14 @@
 package com.its.happy.repository;
 
+import com.its.happy.entity.ProductEntity;
 import com.its.happy.entity.ReviewEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+    List<ReviewEntity> findByMemberEntityMemberId(Long memberId);
+
 }
