8cc756d1a382470136036f057a63f23e8bd4de99
jinseo08 <kjs_8086@naver.com>
2022-07-28 13:40:30 +0900

    메인페이지 css 수정2

--- src/main/java/com/its/happy/controller/MemberController.java
+++ src/main/java/com/its/happy/controller/MemberController.java
@@ -67,7 +67,7 @@ public class MemberController {
         if(loginResult != null){
             session.setAttribute("loginEmail",loginResult.getMemberEmail());
             session.setAttribute("loginId",loginResult.getMemberId());
-            return "index2";
+            return "index";
         } else {
             return "/memberPages/login";
         }
