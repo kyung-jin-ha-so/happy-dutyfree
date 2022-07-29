476315ad49738e4ed2a0e15c2856bd1d9627f407
ktaekim2 <ktaekim2@gmail.com>
2022-07-29 11:59:05 +0900

    - "net::ERR_INCOMPLETE_CHUNKED_ENCODING" 에러 해결-타임리프 문법 내 오타가 원인(memeber) - departure, order의 html의 script 코드를 js파일로 바꾸어 import하는 형식으로 바꿈 - 주문 페이지 출국정보 불러오기 js작업

--- src/main/resources/static/js/iamport.js
+++ src/main/resources/static/js/iamport.js
@@ -1,35 +1,34 @@
+// 아임포트
 const IMP = window.IMP; // 생략 가능
-IMP.init("{imp66752664}"); // 예: imp00000000
-
-function requestPay() {
+IMP.init("imp66752664"); // 예: imp00000000
+function requestPay(pgName, paymentWon) {
     // IMP.request_pay(param, callback) 결제창 호출
     IMP.request_pay({ // param
-        pg: "html5_inicis",
+        pg: pgName,
         pay_method: "card",
-        merchant_uid: "ORD20180131-0000011",
-        name: "노르웨이 회전 의자",
-        amount: 64900,
-        buyer_email: "gildong@gmail.com",
-        buyer_name: "홍길동",
-        buyer_tel: "010-4242-4242",
-        buyer_addr: "서울특별시 강남구 신사동",
-        buyer_postcode: "01181"
+        merchant_uid: 'merchant_' + new Date().getTime(), // 중복되지 않는 값(임의의 값, 주문 고유번호)
+        name: "[[${cartList.get(0).productName}]] 외 [[${cartList.size()} - 1]]개",
+        amount: paymentWon,
+        buyer_email: "[[${member.memberEmail}]]",
+        buyer_name: "[[${member.memberName}]]",
+        buyer_tel: "[[${member.memberMobile}]]",
     }, function (rsp) { // callback
         if (rsp.success) { // 결제 성공 시: 결제 승인 또는 가상계좌 발급에 성공한 경우
             // jQuery로 HTTP 요청
-            jQuery.ajax({
-                url: "{서버의 결제 정보를 받는 endpoint}", // 예: https://www.myservice.com/payments/complete
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                data: {
-                    imp_uid: rsp.imp_uid,
-                    merchant_uid: rsp.merchant_uid
-                }
-            }).done(function (data) {
-                // 가맹점 서버 결제 API 성공시 로직
-            })
+            // jQuery.ajax({
+            //     url: "{서버의 결제 정보를 받는 endpoint}", // 예: https://www.myservice.com/payments/complete
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/json" },
+            //     data: {
+            //         imp_uid: rsp.imp_uid,
+            //         merchant_uid: rsp.merchant_uid
+            //     }
+            // }).done(function (data) {
+            //     // 가맹점 서버 결제 API 성공시 로직
+            // })
+            alert("완료" + rsp.imp_uid + rsp.merchant_uid)
         } else {
-            alert("결제에 실패하였습니다. 에러 내용: " +  rsp.error_msg);
+            alert("결제에 실패하였습니다. 에러 내용: " + rsp.error_msg);
         }
     });
 }
