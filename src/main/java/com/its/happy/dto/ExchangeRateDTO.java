2bb87ef348aeb6a7217acec3fbc3bd4fd421c744
minha723 <99456861+minha723@users.noreply.github.com>
2022-07-29 16:47:36 +0900

    상품가격에 환율 적용 (#44)
    * 상품가격에 환율 적용

--- src/main/java/com/its/happy/dto/ExchangeRateDTO.java
+++ src/main/java/com/its/happy/dto/ExchangeRateDTO.java
@@ -1,5 +1,6 @@
 package com.its.happy.dto;
 
+import com.its.happy.entity.ExchangeRateEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,12 @@ public class ExchangeRateDTO {
     private double exchangeRate;
     private String exchangeRateDate;
 
+    public static ExchangeRateDTO toDto(ExchangeRateEntity exchangeRateEntity) {
+        ExchangeRateDTO exchangeRateDTO = new ExchangeRateDTO();
+        exchangeRateDTO.setExchangeRateId(exchangeRateEntity.getExchangeRateId());
+        double exchangeRate = Math.round( exchangeRateEntity.getExchangeRate() * 100) / 100.0;
+        exchangeRateDTO.setExchangeRate(exchangeRate);
+        exchangeRateDTO.setExchangeRateDate(exchangeRateEntity.getExchangeRateDate());
+        return  exchangeRateDTO;
+    }
 }
