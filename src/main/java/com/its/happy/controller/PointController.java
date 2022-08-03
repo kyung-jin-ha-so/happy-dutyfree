b267958a7fcaee766824dcc887a02e889b9d15cf
jinseo08 <kjs_8086@naver.com>
2022-08-03 15:56:34 +0900

    point 목록 조회 (로그인한 회원 전체 포인트목록)

--- src/main/java/com/its/happy/controller/PointController.java
+++ src/main/java/com/its/happy/controller/PointController.java
@@ -1,11 +1,35 @@
 package com.its.happy.controller;
 
+import com.its.happy.dto.CouponMemberDTO;
+import com.its.happy.dto.PointDTO;
+import com.its.happy.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/point")
 public class PointController {
+
+    final PointService pointService;
+
+    @GetMapping("/findByMemberId")
+    public String pointFindAll(HttpSession session,Model model){
+        Long memberId = (Long) session.getAttribute("loginId");
+        List<PointDTO> pointDTOList = pointService.findByPoint(memberId);
+        model.addAttribute("pointList",pointDTOList);
+        return "memberPages/pointList";
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/its/happy/dto/PointDTO.java
+++ src/main/java/com/its/happy/dto/PointDTO.java
@@ -1,12 +1,14 @@
 package com.its.happy.dto;
 
 import com.its.happy.entity.MemberEntity;
+import com.its.happy.entity.PointEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +20,11 @@ public class PointDTO {
     private int pointValue;
     private LocalDateTime PointCreatedTime;
 
+    public static PointDTO toPointDTO(PointEntity pointEntity) {
+        PointDTO pointDTO = new PointDTO();
+        pointDTO.setPointId(pointEntity.getPointId());
+        pointDTO.setPointValue(pointEntity.getPointValue());
+        pointDTO.setPointCreatedTime(pointEntity.getCreatedTime());
+        return pointDTO;
+    }
 }
