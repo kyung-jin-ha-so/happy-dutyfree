98bd9250c7208c761573aa0e6378ee4c6e4c084b
ktaekim2 <ktaekim2@gmail.com>
2022-08-07 23:44:12 +0900

    - 'finalWon'의 계산 실수 수정 - 사용 가능 적립금 타임리프 수정 - order_table 수정 - order_product_table 수정 - payment_table 삭제 - 아임포트 수정 중

--- src/main/java/com/its/happy/entity/OrderProductEntity.java
+++ src/main/java/com/its/happy/entity/OrderProductEntity.java
@@ -10,19 +10,23 @@ import javax.persistence.*;
 @Setter
 @Table(name = "order_product_table")
 public class OrderProductEntity {
+    // 주문상품 번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long orderProductId;
 
+    // 상품 정가
+    @Column(nullable = false)
+    private double productOriginalPrice;
+
+    // 상품 할인율
+    @Column(nullable = false)
+    private Long productDiscount;
+
+    // 주문 수량
     @Column(nullable = false)
     private int orderQty;
-
-    @Column(nullable = false)
-    private double orderWon;
-
-    @Column(nullable = false)
-    private double orderDollar;
 
     // OrderProductEntity(N)가 OrderEntity(1)을 참조함
     @ManyToOne(fetch = FetchType.LAZY)
