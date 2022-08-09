22d83aa0051598850cbb94b06714af632fa9fd67
ktaekim2 <ktaekim2@gmail.com>
2022-08-09 15:02:29 +0900

    - OrderProductEntity에 컬럼 2개 추가(이름, 할인가) - OrderDTO에 필드2개 추가(orderProductEntityList, orderDepartureEntity) - 주문내역 페이지 완성 - 주문 취소 기능 완성 - 주문 상세내역 페이지 부분완성

--- src/main/java/com/its/happy/dto/OrderDTO.java
+++ src/main/java/com/its/happy/dto/OrderDTO.java
@@ -1,12 +1,15 @@
 package com.its.happy.dto;
 
+import com.its.happy.entity.OrderDepartureEntity;
 import com.its.happy.entity.OrderEntity;
+import com.its.happy.entity.OrderProductEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Data
@@ -25,6 +28,8 @@ public class OrderDTO {
     private double exchangeRate;
     private Long couponMemberId;
     private Long memberId;
+    private List<OrderProductEntity> orderProductEntityList;
+    private OrderDepartureEntity orderDepartureEntity;
 
     public static OrderDTO toDTO(OrderEntity orderEntity) {
         OrderDTO orderDTO = new OrderDTO();
@@ -39,7 +44,28 @@ public class OrderDTO {
         orderDTO.setCouponUseValue(orderEntity.getCouponUseValue());
         orderDTO.setExchangeRate(orderEntity.getExchangeRate());
         orderDTO.setCouponMemberId(orderEntity.getCouponMemberEntity().getCouponMemberId());
-        orderDTO.setMemberId(orderEntity.getCouponMemberEntity().getMemberEntity().getMemberId());
+        orderDTO.setMemberId(orderEntity.getMemberEntity().getMemberId());
+        orderDTO.setOrderProductEntityList(orderEntity.getOrderProductEntityList());
+        orderDTO.setOrderDepartureEntity(orderEntity.getOrderDepartureEntity());
+        return orderDTO;
+    }
+
+    // CouponMemberEntity가 null일 경우
+    public static OrderDTO toDTO2(OrderEntity orderEntity) {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setOrderId(orderEntity.getOrderId());
+        orderDTO.setOrderCreatedTime(orderEntity.getCreatedTime());
+        orderDTO.setOrderUpdatedTime(orderEntity.getUpdatedTime());
+        orderDTO.setOrderStatus(orderEntity.getOrderStatus());
+        orderDTO.setNetDcWon(orderEntity.getNetDcWon());
+        orderDTO.setOrderUsd(orderEntity.getOrderUsd());
+        orderDTO.setOrderWon(orderEntity.getOrderWon());
+        orderDTO.setPointUseValue(orderEntity.getPointUseValue());
+        orderDTO.setCouponUseValue(orderEntity.getCouponUseValue());
+        orderDTO.setExchangeRate(orderEntity.getExchangeRate());
+        orderDTO.setMemberId(orderEntity.getMemberEntity().getMemberId());
+        orderDTO.setOrderProductEntityList(orderEntity.getOrderProductEntityList());
+        orderDTO.setOrderDepartureEntity(orderEntity.getOrderDepartureEntity());
         return orderDTO;
     }
 }
