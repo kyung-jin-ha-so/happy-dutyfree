62df160208a73b89ca732b77d764ff09546f5a44
minha723 <99456861+minha723@users.noreply.github.com>
2022-08-10 14:59:55 +0900

    상품 후기 및 별점 관련 (#80)
    * 상품 후기 및 별점 등록
    * 상품 후기 및 별점 css 수정

--- src/main/java/com/its/happy/controller/ReviewController.java
+++ src/main/java/com/its/happy/controller/ReviewController.java
@@ -29,7 +29,7 @@ public class ReviewController {
     private final ReviewService reviewService;
     private final OrderProductService orderProductService;
 
-    //orderProduct테이블에서 상품 관련 정보 가지고 오기
+    //후기 저장하는 폼 보여주기
     @GetMapping("/save/{orderProductId}")
     public String saveForm(@PathVariable Long orderProductId, Model model){
         OrderProductEntity orderProductEntity = orderProductService.findById(orderProductId);
@@ -38,19 +38,15 @@ public class ReviewController {
         model.addAttribute("orderId", orderProductEntity.getOrderEntity().getOrderId());
         return "/reviewPages/save";
     }
+    //상품 후기 저장
     @PostMapping("/save")
     public String save(@ModelAttribute ReviewDTO reviewDTO, @RequestParam Long productId, @RequestParam Long memberId,
                        @RequestParam Long orderId){
         reviewService.save(reviewDTO, memberId, productId, orderId);
-        return "redirect: /review/findByMemberId/" + memberId;
+        return "redirect:/review/findByMemberId/" + memberId;
     }
 
-    //save form테스트용
-    @GetMapping("/saveFormTest")
-    public String save(){
-        return "/reviewPages/save";
-    }
-
+    //나의 작성 후기 보기
     @GetMapping("/findByMemberId/{memberId}")
     public String findByMemberId(@PathVariable Long memberId, Model model){
         List<ReviewDTO> reviewDTOList = reviewService.findByMemberId(memberId);
@@ -58,26 +54,28 @@ public class ReviewController {
         return "/reviewPages/list";
     }
 
-    //후기 목록 test 용
-    @GetMapping("/list")
-    public String list(){
-        return "/reviewPages/list";
-    }
-
-    //후기 삭제 test중
-    @GetMapping("/delete/{reviewId}")
-    public String reviewDelete(@PathVariable Long reviewId, HttpSession session){
-        reviewService.deleteById(reviewId);
-        Long memberId = (Long) session.getAttribute("loginId");
-        return "redirect: /review/findByMemberId/" + memberId;
-    }
-
-    //후기 수정 test중
+    //후기 수정 form 보여주기
     @GetMapping("/update/{reviewId}")
     public String updateForm(@PathVariable Long reviewId, Model model){
         ReviewDTO reviewDTO = reviewService.findById(reviewId);
         model.addAttribute("review", reviewDTO);
         return "/reviewPages/update";
     }
+
+    //후기 수정하기
+    @PostMapping("/update")
+    public String update(@ModelAttribute ReviewDTO reviewDTO,@RequestParam Long productId, @RequestParam Long memberId, HttpSession session){
+        reviewService.update(reviewDTO, productId);
+        return "redirect:/review/findByMemberId/" + memberId;
+    }
+
+    //후기 삭제
+    @GetMapping("/delete/{reviewId}")
+    public String reviewDelete(@PathVariable Long reviewId,@RequestParam Long productId, HttpSession session){
+        reviewService.deleteById(reviewId, productId);
+        Long memberId = (Long) session.getAttribute("loginId");
+        return "redirect:/review/findByMemberId/" + memberId;
+    }
+
 
 }
--- src/main/java/com/its/happy/repository/ReviewRepository.java
+++ src/main/java/com/its/happy/repository/ReviewRepository.java
@@ -5,10 +5,16 @@ import com.its.happy.entity.ReviewEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     List<ReviewEntity> findByMemberEntityMemberId(Long memberId);
+
+    @Query(value = "select avg(r.reviewStar) from ReviewEntity r where r.productEntity.productId = :productId")
+    Double findAvg(@Param("productId") Long productId);
 
 }
