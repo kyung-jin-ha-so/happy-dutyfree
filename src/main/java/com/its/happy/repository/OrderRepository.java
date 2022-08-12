91321118aaec02dd28088c686fa08eed90d608e9
minha723 <minhakim0723@naver.com>
2022-08-12 15:15:51 +0900

    오늘의 매출 계산 기능 추가

--- src/main/java/com/its/happy/repository/OrderRepository.java
+++ src/main/java/com/its/happy/repository/OrderRepository.java
@@ -5,9 +5,14 @@ import com.its.happy.entity.MemberEntity;
 import com.its.happy.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     List<OrderEntity> findByMemberEntityMemberId(Long memberId);
+
+//    오늘의 주문 명단 뽑기
+    List<OrderEntity> findAllByCreatedTimeBetween(LocalDateTime start, LocalDateTime end);
 }
